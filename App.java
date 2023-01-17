5f21a85869a26c8e14c20d75e37eefa93d894e7d
Darryl Ng <darryl1975@hotmail.com>
2023-01-17 15:38:09 +0800

    added code to send outputstream

--- App.java
+++ App.java
@@ -1,12 +1,15 @@
 package sg.edu.nus.iss;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -27,7 +30,7 @@ public final class App {
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
         String dirPath = "data2";
-        
+
         // instantiate a file/directory object
         File newDirectory = new File(dirPath);
 
@@ -43,7 +46,7 @@ public final class App {
         cookie.readCookieFile();
         cookie.showCookies();
 
-        ServerSocket ss = new ServerSocket(7000);
+        ServerSocket ss = new ServerSocket(12345);
         Socket s = ss.accept(); // establish connection and wait for client to connect
 
         try (InputStream is = s.getInputStream()) {
@@ -51,20 +54,36 @@ public final class App {
             DataInputStream dis = new DataInputStream(bis);
             String msgReceived = "";
 
-            while(!msgReceived.equals("close")) {
-                msgReceived = dis.readUTF();
+            try (OutputStream os = s.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
 
-                if (msgReceived.equalsIgnoreCase("get-cookie")) {
-                    String cookieValue = cookie.returnCookie();
-                    System.out.println(cookieValue);
+                while (!msgReceived.equals("close")) {
+                    msgReceived = dis.readUTF();
+
+                    if (msgReceived.equalsIgnoreCase("get-cookie")) {
+                        String cookieValue = cookie.returnCookie();
+                        System.out.println(cookieValue);
+
+                        dos.writeUTF(cookieValue);
+                        dos.flush();
+                    }
                 }
+
+                dos.close();
+                bos.close();
+                os.close();
+            } catch (EOFException ex) {
+                ex.printStackTrace();
             }
+
+            bis.close();
+            dis.close();
+            is.close();
         } catch (EOFException ex) {
             s.close();
             ss.close();
         }
 
-        
-        
     }
 }
